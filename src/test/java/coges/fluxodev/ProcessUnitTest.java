49d273d915e3fe72aa5a01615afc71f4deeb7f69
Victoria <victoria.botelho@posgrad.ufsc.br>
2023-03-08 18:36:42 -0300

    fix: assertion test

--- src/test/java/coges/fluxodev/ProcessUnitTest.java
+++ src/test/java/coges/fluxodev/ProcessUnitTest.java
@@ -36,10 +36,10 @@ public class ProcessUnitTest {
 	public void testHappyPath() {
 		// Drive the process by API and assert correct behavior by camunda-bpm-assert
 
-//    ProcessInstance processInstance = processEngine().getRuntimeService()
-//        .startProcessInstanceByKey(ProcessConstants.PROCESS_DEFINITION_KEY);
-//
-//    assertThat(processInstance).isEnded();
+		ProcessInstance processInstance = processEngine().getRuntimeService()
+				.startProcessInstanceByKey(ProcessConstants.PROCESS_DEFINITION_KEY);
+
+		assertThat(processInstance).isActive();
 	}
 
 }
